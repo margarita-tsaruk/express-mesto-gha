f7dfb7f71e82fcce229baa14581ac66c1ba73d62
margarita-tsaruk <margarita.tsaruk@yandex.ru>
2022-08-28 12:00:58 +0200

    fix - auto test 3

--- routes/cards.js
+++ routes/cards.js
@@ -10,14 +10,14 @@ const {
   dislikeCard,
 } = require('../controllers/cards');
 
-cardRoutes.get('/cards', express.json(), getCards);
+cardRoutes.get('/cards', getCards);
 
-cardRoutes.post('/cards', express.json(), createCards);
+cardRoutes.post('/cards', createCards);
 
-cardRoutes.delete('/cards/:cardId', express.json(), deleteCard);
+cardRoutes.delete('/cards/:cardId', deleteCard);
 
-cardRoutes.put('/cards/:cardId/likes', express.json(), likeCard);
+cardRoutes.put('/cards/:cardId/likes', likeCard);
 
-cardRoutes.delete('/cards/:cardId/likes', express.json(), dislikeCard);
+cardRoutes.delete('/cards/:cardId/likes', dislikeCard);
 
 module.exports = cardRoutes;
